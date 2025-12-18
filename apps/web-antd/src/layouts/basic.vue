713befcdee076020835149f01f97b379f664c7b1
Otter-BlueJay <464485682@qq.com>
2025-12-18 15:56:35 +0800

    feat: 隐藏通知

--- apps/web-antd/src/layouts/basic.vue
+++ apps/web-antd/src/layouts/basic.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
-import type { NotificationItem } from '@vben/layouts';
+// import type { NotificationItem } from '@vben/layouts';
 
-import { computed, ref, watch } from 'vue';
+import { computed, watch } from 'vue';
 
 import { AuthenticationLoginExpiredModal } from '@vben/common-ui';
 import { useWatermark } from '@vben/hooks';
@@ -9,7 +9,7 @@ import { useWatermark } from '@vben/hooks';
 import {
   BasicLayout,
   LockScreen,
-  Notification,
+  // Notification,
   UserDropdown,
 } from '@vben/layouts';
 import { preferences } from '@vben/preferences';
@@ -18,67 +18,67 @@ import { useAccessStore, useUserStore } from '@vben/stores';
 import { useAuthStore } from '#/store';
 import LoginForm from '#/views/_core/authentication/login.vue';
 
-const notifications = ref<NotificationItem[]>([
-  {
-    id: 1,
-    avatar: 'https://avatar.vercel.sh/vercel.svg?text=VB',
-    date: '3小时前',
-    isRead: true,
-    message: '描述信息描述信息描述信息',
-    title: '收到了 14 份新周报',
-  },
-  {
-    id: 2,
-    avatar: 'https://avatar.vercel.sh/1',
-    date: '刚刚',
-    isRead: false,
-    message: '描述信息描述信息描述信息',
-    title: '朱偏右 回复了你',
-  },
-  {
-    id: 3,
-    avatar: 'https://avatar.vercel.sh/1',
-    date: '2024-01-01',
-    isRead: false,
-    message: '描述信息描述信息描述信息',
-    title: '曲丽丽 评论了你',
-  },
-  {
-    id: 4,
-    avatar: 'https://avatar.vercel.sh/satori',
-    date: '1天前',
-    isRead: false,
-    message: '描述信息描述信息描述信息',
-    title: '代办提醒',
-  },
-  {
-    id: 5,
-    avatar: 'https://avatar.vercel.sh/satori',
-    date: '1天前',
-    isRead: false,
-    message: '描述信息描述信息描述信息',
-    title: '跳转Workspace示例',
-    link: '/workspace',
-  },
-  {
-    id: 6,
-    avatar: 'https://avatar.vercel.sh/satori',
-    date: '1天前',
-    isRead: false,
-    message: '描述信息描述信息描述信息',
-    title: '跳转外部链接示例',
-    link: 'https://doc.vben.pro',
-  },
-]);
+// const notifications = ref<NotificationItem[]>([
+//   {
+//     id: 1,
+//     avatar: 'https://avatar.vercel.sh/vercel.svg?text=VB',
+//     date: '3小时前',
+//     isRead: true,
+//     message: '描述信息描述信息描述信息',
+//     title: '收到了 14 份新周报',
+//   },
+//   {
+//     id: 2,
+//     avatar: 'https://avatar.vercel.sh/1',
+//     date: '刚刚',
+//     isRead: false,
+//     message: '描述信息描述信息描述信息',
+//     title: '朱偏右 回复了你',
+//   },
+//   {
+//     id: 3,
+//     avatar: 'https://avatar.vercel.sh/1',
+//     date: '2024-01-01',
+//     isRead: false,
+//     message: '描述信息描述信息描述信息',
+//     title: '曲丽丽 评论了你',
+//   },
+//   {
+//     id: 4,
+//     avatar: 'https://avatar.vercel.sh/satori',
+//     date: '1天前',
+//     isRead: false,
+//     message: '描述信息描述信息描述信息',
+//     title: '代办提醒',
+//   },
+//   {
+//     id: 5,
+//     avatar: 'https://avatar.vercel.sh/satori',
+//     date: '1天前',
+//     isRead: false,
+//     message: '描述信息描述信息描述信息',
+//     title: '跳转Workspace示例',
+//     link: '/workspace',
+//   },
+//   {
+//     id: 6,
+//     avatar: 'https://avatar.vercel.sh/satori',
+//     date: '1天前',
+//     isRead: false,
+//     message: '描述信息描述信息描述信息',
+//     title: '跳转外部链接示例',
+//     link: 'https://doc.vben.pro',
+//   },
+// ]);
 
 // const router = useRouter();
 const userStore = useUserStore();
 const authStore = useAuthStore();
 const accessStore = useAccessStore();
 const { destroyWatermark, updateWatermark } = useWatermark();
-const showDot = computed(() =>
-  notifications.value.some((item) => !item.isRead),
-);
+// const showDot = computed(() =>
+//   notifications.value.some((item) => !item.isRead),
+// );
 
 const menus = computed(() => [
   // {
@@ -118,24 +118,24 @@ async function handleLogout() {
   await authStore.logout(false);
 }
 
-function handleNoticeClear() {
-  notifications.value = [];
-}
+// function handleNoticeClear() {
+//   notifications.value = [];
+// }
 
-function markRead(id: number | string) {
-  const item = notifications.value.find((item) => item.id === id);
-  if (item) {
-    item.isRead = true;
-  }
-}
+// function markRead(id: number | string) {
+//   const item = notifications.value.find((item) => item.id === id);
+//   if (item) {
+//     item.isRead = true;
+//   }
+// }
 
-function remove(id: number | string) {
-  notifications.value = notifications.value.filter((item) => item.id !== id);
-}
+// function remove(id: number | string) {
+//   notifications.value = notifications.value.filter((item) => item.id !== id);
+// }
 
-function handleMakeAll() {
-  notifications.value.forEach((item) => (item.isRead = true));
-}
+// function handleMakeAll() {
+//   notifications.value.forEach((item) => (item.isRead = true));
+// }
 watch(
   () => ({
     enable: preferences.app.watermark,
@@ -170,7 +170,7 @@ watch(
         @logout="handleLogout"
       />
     </template>
-    <template #notification>
+    <!-- <template #notification>
       <Notification
         :dot="showDot"
         :notifications="notifications"
@@ -179,7 +179,7 @@ watch(
         @remove="(item) => item.id && remove(item.id)"
         @make-all="handleMakeAll"
       />
-    </template>
+    </template> -->
     <template #extra>
       <AuthenticationLoginExpiredModal
         v-model:open="accessStore.loginExpired"
