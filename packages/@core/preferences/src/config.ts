aa996e1337bd0ed8574475c47b29fc04b2dfb314
roy <464485682@qq.com>
2026-03-20 15:53:48 +0800

    fix(project): 改用本地 Logo 资源

--- packages/@core/preferences/src/config.ts
+++ packages/@core/preferences/src/config.ts
@@ -65,7 +65,7 @@ const defaultPreferences: Preferences = {
   logo: {
     enable: true,
     fit: "contain",
-    source: "https://unpkg.com/@vbenjs/static-source@0.1.7/source/logo-v1.webp",
+    source: "/logo.png",
   },
   navigation: {
     accordion: true,
